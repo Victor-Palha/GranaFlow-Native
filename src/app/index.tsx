57d664423409a658b9c74151453e0e43a0b081d5
strahd <jv.palha@gmail.com>
2025-03-07 16:44:52 -0300

    add: google photo to button login

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index(){
     return (
-        <View>
-            <Text className="text-4xl text-green-high">Hello World</Text>
+        <View className="flex-1 items-center justify-center gap-10">
+            <View className="gap-4 items-center">
+                <Image source={require("../assets/logo.png")}/>
+                <Text className="text-4xl shadow-black shadow">Grana<Text className="text-green-low">Flow</Text></Text>
+            </View>
+            <TouchableOpacity className="flex-row items-center gap-5 bg-white w-[60%] justify-center rounded-lg p-3 shadow">
+                <Image source={require("../assets/Google.png")}/>
+                <Text className="text-xl">Entrar com Google</Text>
+            </TouchableOpacity>
         </View>
     )
 }
